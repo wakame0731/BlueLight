38bcff492c32ddfcb91005af4528afa6bd556ec8
WAKAME <wakame186@gmail.com>
2017-10-11 18:10:49 +0900

    invalidMoveを無効化

--- src/pocketmine/Player.php
+++ src/pocketmine/Player.php
@@ -1640,6 +1640,7 @@ class Player extends Human implements CommandSender, ChunkLoader, IPlayer{
 
 			$diff = ($diffX ** 2 + $diffY ** 2 + $diffZ ** 2) / ($tickDiff ** 2);
 
+			/*
 			if($this->isSurvival() and !$revert and $diff > 0.0625){
 				$ev = new PlayerIllegalMoveEvent($this, $newPos);
 				$ev->setCancelled($this->allowMovementCheats);
@@ -1651,7 +1652,7 @@ class Player extends Human implements CommandSender, ChunkLoader, IPlayer{
 					$this->server->getLogger()->warning($this->getServer()->getLanguage()->translateString("pocketmine.player.invalidMove", [$this->getName()]));
 					$this->server->getLogger()->debug("Old position: " . $this->asVector3() . ", new position: " . $this->newPosition);
 				}
-			}
+			}*/
 
 			if($diff > 0){
 				$this->x = $newPos->x;
