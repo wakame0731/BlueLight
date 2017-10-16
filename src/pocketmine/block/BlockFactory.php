997bc7dcfe6ecb89c925a4ad49b19da43a584510
WAKAME <wakame186@gmail.com>
2017-10-16 09:59:32 +0900

    赤砂のレンガ系追加

--- src/pocketmine/block/BlockFactory.php
+++ src/pocketmine/block/BlockFactory.php
@@ -249,10 +249,9 @@ class BlockFactory{
 			//TODO: STANDING_BANNER
 			//TODO: WALL_BANNER
 			//TODO: DAYLIGHT_DETECTOR_INVERTED
-			//TODO: RED_SANDSTONE
-			//TODO: RED_SANDSTONE_STAIRS
+			self::registerBlock(new RedSandstone());
 			//TODO: DOUBLE_STONE_SLAB2
-			//TODO: STONE_SLAB2
+			self::registerBlock(new RedSandstoneSlab());
 			self::registerBlock(new FenceGate(Block::SPRUCE_FENCE_GATE, 0, "Spruce Fence Gate"));
 			self::registerBlock(new FenceGate(Block::BIRCH_FENCE_GATE, 0, "Birch Fence Gate"));
 			self::registerBlock(new FenceGate(Block::JUNGLE_FENCE_GATE, 0, "Jungle Fence Gate"));
