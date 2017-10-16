7cb71cbdc4fca111d636abb81d4c416f3c62f192
WAKAME <wakame186@gmail.com>
2017-10-16 10:20:30 +0900

    スポンジが機能するように

--- src/pocketmine/block/Sponge.php
+++ src/pocketmine/block/Sponge.php
@@ -22,11 +22,13 @@
 declare(strict_types=1);
 
 namespace pocketmine\block;
-
+use pocketmine\level\Level;
+use pocketmine\math\Vector3;
 
 class Sponge extends Solid{
 
 	protected $id = self::SPONGE;
+	protected $absorbRange = 6;
 
 	public function __construct(int $meta = 0){
 		$this->meta = $meta;
@@ -37,7 +39,63 @@ class Sponge extends Solid{
 	}
 
 	public function getName() : string{
-		return "Sponge";
+		static $names = [
+			0 => "Sponge",
+			1 => "Wet Sponge",
+		];
+		return $names[$this->meta & 0x0f];
 	}
-
+	public function absorbWater(){
+		$range = $this->absorbRange / 2;
+		for($xx = -$range; $xx <= $range; $xx++){
+			for($yy = -$range; $yy <= $range; $yy++){
+				for($zz = -$range; $zz <= $range; $zz++){
+					$block = $this->getLevel()->getBlock(new Vector3($this->x + $xx, $this->y + $yy, $this->z + $zz));
+					if($block->getId() === Block::WATER) $this->getLevel()->setBlock($block, Block::get(Block::AIR), true, true);
+					if($block->getId() === Block::STILL_WATER) $this->getLevel()->setBlock($block, Block::get(Block::AIR), true, true);
+				}
+			}
+		}
+	}
+	/**
+	 * @param int $type
+	 *
+	 * @return bool|int
+	 */
+	public function onUpdate($type){
+		if($this->meta == 0){
+			if($type === Level::BLOCK_UPDATE_NORMAL){
+				$blockAbove = $this->getSide(Vector3::SIDE_UP)->getId();
+				$blockBeneath = $this->getSide(Vector3::SIDE_DOWN)->getId();
+				$blockNorth = $this->getSide(Vector3::SIDE_NORTH)->getId();
+				$blockSouth = $this->getSide(Vector3::SIDE_SOUTH)->getId();
+				$blockEast = $this->getSide(Vector3::SIDE_EAST)->getId();
+				$blockWest = $this->getSide(Vector3::SIDE_WEST)->getId();
+				if($blockAbove === Block::WATER ||
+					$blockBeneath === Block::WATER ||
+					$blockNorth === Block::WATER ||
+					$blockSouth === Block::WATER ||
+					$blockEast === Block::WATER ||
+					$blockWest === Block::WATER
+				){
+					$this->absorbWater();
+					$this->getLevel()->setBlock($this, Block::get(Block::SPONGE, 1), true, true);
+					return Level::BLOCK_UPDATE_NORMAL;
+				}
+				if($blockAbove === Block::STILL_WATER ||
+					$blockBeneath === Block::STILL_WATER ||
+					$blockNorth === Block::STILL_WATER ||
+					$blockSouth === Block::STILL_WATER ||
+					$blockEast === Block::STILL_WATER ||
+					$blockWest === Block::STILL_WATER
+				){
+					$this->absorbWater();
+					$this->getLevel()->setBlock($this, Block::get(Block::SPONGE, 1), true, true);
+					return Level::BLOCK_UPDATE_NORMAL;
+				}
+			}
+			return false;
+		}
+		return true;
+	}
 }
